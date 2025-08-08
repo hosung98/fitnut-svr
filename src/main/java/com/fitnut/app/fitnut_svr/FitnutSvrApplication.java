2bcfbc1191c3ba60ea85835f639ace2f04a2c8c2
cosdev40 <cosdev40@cosmo.cup>
2025-08-08 10:05:01 +0900

    초기 spring 구성

---
+++ src/main/java/com/fitnut/app/fitnut_svr/FitnutSvrApplication.java
@@ -0,0 +1,16 @@
+package com.fitnut.app.fitnut_svr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
+public class FitnutSvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnutSvrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitnut/app/fitnut_svr/FitnutSvrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnut.app.fitnut_svr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnutSvrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
