11379599ac4562822beadf9acc47558921c6c841
kimhosung <70985410+hosung98@users.noreply.github.com>
2025-08-11 16:50:14 +0900

    도커파일 초기 구성

--- src/main/java/com/fitnut/app/user/Controller/LoginController.java
+++ src/main/java/com/fitnut/app/user/Controller/LoginController.java
@@ -1,4 +1,10 @@
 package com.fitnut.app.user.Controller;
 
+import com.fitnut.app.user.Dto.LoginDto;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LoginController {
+
+
 }
